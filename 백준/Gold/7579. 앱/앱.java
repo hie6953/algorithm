cd201571ea5e5b3945c13de87da621e57941dc41
Jonghui <68373532+hie6953@users.noreply.github.com>
2022-11-17 00:26:39 +0900

    [Gold III] Title: 앱, Time: 144 ms, Memory: 14584 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -8,16 +8,17 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[] bites = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] costs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        StringTokenizer bites = new StringTokenizer(br.readLine());
+        StringTokenizer costs = new StringTokenizer(br.readLine());
         int[] dp = new int[MAX_MEMORY];
         int index = 0;
         for (int n = 0; n < N; n++) {
-            if (costs[n] == 0) {
-                M -= bites[n];
+            int bite = Integer.parseInt(bites.nextToken());
+            int cost = Integer.parseInt(costs.nextToken());
+            if (cost == 0) {
+                M -= bite;
                 continue;
             }
-            int cost = costs[n], bite = bites[n];
             for (int i = index; i > 0; i--) {
                 if (dp[i] != 0) dp[i + cost] = Math.max(dp[i + cost], dp[i] + bite);
             }

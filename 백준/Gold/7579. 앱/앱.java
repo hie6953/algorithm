289a9feaf4787eeea9416f9d18fc69c12f98a7d1
Jonghui <68373532+hie6953@users.noreply.github.com>
2022-11-17 00:11:36 +0900

    [Gold III] Title: 앱, Time: 144 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -10,27 +10,22 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
         int[] bites = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[] costs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] prev = new int[MAX_MEMORY];
+        int[] dp = new int[MAX_MEMORY];
         int index = 0;
         for (int n = 0; n < N; n++) {
             if (costs[n] == 0) {
                 M -= bites[n];
                 continue;
             }
-            int[] now = new int[MAX_MEMORY];
             int cost = costs[n], bite = bites[n];
             for (int i = index; i > 0; i--) {
-                if (prev[i] != 0) {
-                    now[i + cost] = Math.max(now[i + cost], prev[i] + bite);
-                    now[i] = prev[i];
-                }
+                if (dp[i] != 0) dp[i + cost] = Math.max(dp[i + cost], dp[i] + bite);
             }
-            now[cost] = Math.max(now[cost], bite);
+            dp[cost] = Math.max(dp[cost], bite);
             index += cost;
-            prev = now;
         }
         for (int i = 0; i < MAX_MEMORY; i++) {
-            if (prev[i] >= M) {
+            if (dp[i] >= M) {
                 System.out.println(i);
                 break;
             }
